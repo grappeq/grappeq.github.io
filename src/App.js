9d9b602e6c033f20343d8a30a605c2415c8757a7
Kacper Grabowski <kacperg333@gmail.com>
2021-05-16 19:54:04 +0200

    cleanup styling and get rid of bulma from dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import * as chroma from 'chroma-js';
 import './App.css';
 import photo from './photo.jpg';
@@ -19,7 +19,7 @@ class App extends Component {
     constructor() {
         super();
 
-        const  { backgroundColor, foregroundColor } = this.generateNiceContrastingColors();
+        const { backgroundColor, foregroundColor } = this.generateNiceContrastingColors();
         this.state = {
             emailAddressHidden: true,
             backgroundColor,
@@ -36,7 +36,7 @@ class App extends Component {
 
     generateNiceContrastingColors() {
         while (true) {
-            const  { backgroundColor, foregroundColor } = this.generateMatchingColors();
+            const { backgroundColor, foregroundColor } = this.generateMatchingColors();
             if (chroma.contrast(backgroundColor, foregroundColor) > MIN_CONTRAST_RATIO) {
                 return { backgroundColor, foregroundColor };
             }
@@ -46,7 +46,7 @@ class App extends Component {
     generateMatchingColors() {
         const backgroundColor = chroma.random().saturate(2).luminance(0.8);
         const [hue, saturation, lightness] = backgroundColor.hsl();
-        const foregroundColor = chroma.hsl(hue+180%360, saturation, lightness);
+        const foregroundColor = chroma.hsl(hue + 180 % 360, saturation, lightness);
 
         return { backgroundColor, foregroundColor };
     }
@@ -66,7 +66,7 @@ class App extends Component {
     photoStyle() {
         const [hueOfMyFace] = MY_FACE_REFERENCE_COLOR.hsv();
         const [foregroundColorHue] = this.state.foregroundColor.hsv();
-        const hueDiff = foregroundColorHue-hueOfMyFace;
+        const hueDiff = foregroundColorHue - hueOfMyFace;
 
         return {
             filter: `hue-rotate(${hueDiff}deg) saturate(200%)`
@@ -75,60 +75,47 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <section className="hero is-fullheight is-light" style={this.backgroundStyle()}>
-                    <div className="hero-body">
-                        <div className="container central-text">
-                            <img
-                                src={photo}
-                                className="photo"
-                                style={this.photoStyle()}
-                                alt="Me"
-                                onClick={this.regenerateColors}
-                            />
-                            <h2 className="name title is-2" style={this.textStyle()}>
-                                Kacper Grabowski
-                            </h2>
-                            <h4 className="position subtitle is-4" style={this.textStyle()}>
-                                Software Engineer
-                            </h4>
-                            <h5 className={`email subtitle is-5${this.state.emailAddressHidden ? "" : " expanded"}`}>
-                                <FontAwesomeIcon
-                                    icon={['fas', 'envelope']}
-                                    className="email-icon"
-                                    onClick={() => {
-                                        this.setState({ emailAddressHidden: false });
-                                    }}
-                                    style={this.textStyle()}
-                                />
-                                {!this.state.emailAddressHidden && <div className="email-text">
-                                    <a href={`mailto:${EMAIL_ADDRESS}`} style={this.textStyle()}>
-                                        {EMAIL_ADDRESS}
-                                    </a>
-                                </div>}
-                            </h5>
-                        </div>
+            <div className="app" style={this.backgroundStyle()}>
+                <div className="app-body">
+                    <img
+                        src={photo}
+                        className="photo"
+                        style={this.photoStyle()}
+                        alt="Me"
+                        onClick={this.regenerateColors}
+                    />
+                    <div className="name" style={this.textStyle()}>
+                        Kacper Grabowski
                     </div>
+                    <div className="position" style={this.textStyle()}>
+                        Software Engineer
+                    </div>
+                    <div className={`email ${this.state.emailAddressHidden ? "" : " expanded"}`}>
+                        <FontAwesomeIcon
+                            icon={['fas', 'envelope']}
+                            className="email-icon"
+                            onClick={() => {
+                                this.setState({ emailAddressHidden: !this.state.emailAddressHidden });
+                            }}
+                            style={this.textStyle()}
+                        />
+                        {!this.state.emailAddressHidden && <div className="email-text">
+                            <a href={`mailto:${EMAIL_ADDRESS}`} style={this.textStyle()}>
+                                {EMAIL_ADDRESS}
+                            </a>
+                        </div>}
+                    </div>
+                </div>
 
-                    <div className="hero-foot">
-                        <div className="container">
-                            <div className="tabs is-centered">
-                                <ul className="links">
-                                    <li>
-                                        <a href="https://www.linkedin.com/in/grabowskikacper/">
-                                            <FontAwesomeIcon icon={['fab', 'linkedin']} style={this.textStyle()} />
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://github.com/grappeq">
-                                            <FontAwesomeIcon icon={['fab', 'github']} style={this.textStyle()} />
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <div className="app-footer">
+                    <a href="https://www.linkedin.com/in/grabowskikacper/">
+                        <FontAwesomeIcon icon={['fab', 'linkedin']} style={this.textStyle()}/>
+                    </a>
+
+                    <a href="https://github.com/grappeq">
+                        <FontAwesomeIcon icon={['fab', 'github']} style={this.textStyle()}/>
+                    </a>
+                </div>
             </div>
         );
     }
